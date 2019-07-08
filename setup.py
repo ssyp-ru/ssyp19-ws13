214da06f7ae69376c34ec184234a602a5f36a181
vsevolod <asd@qwe.com>
2019-07-08 19:19:19 +0700

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Geom',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='ws13',
+    author_email='',
+    description=''
+)
