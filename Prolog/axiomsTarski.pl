7c96262c9627aced3aa7cfc5afa43a81897e2cb4
serg <sdfghjk@yandex.ru>
2019-07-09 19:44:25 +0700

    start permuting

--- Prolog/axiomsTarski.pl
+++ Prolog/axiomsTarski.pl
@@ -1,93 +1,105 @@
 %:- dynamic изменяет предикат во время его чтения, вставляет факт
-/*
-permute(Pred, A, B) :-
-	Perm1 =.. [Pred, A, B],
-	assert(Perm1),
-	Perm2 =.. [Pred, B, A],
-	assert(Perm2).
 
-permute(Pred, A, B, C) :-
-	Perm1 =.. [Pred, A, B, C],
-	assert(Perm1),
-	Perm2 =.. [Pred, B, C, A],
-	assert(Perm2).
-*/
+permuteOrCongruence(Condition, A, C):-
+			AC =.. [Condition, A, C],
+			CA =.. [Condition, C, A],
+			call(AC), !; call(CA), !.
+
+permuteOrLays(Condition, A, B, C):-
+			ABC =.. [Condition, A, B, C],
+			BAC =.. [Condition, B, A, C],
+			call(ABC), !; call(BAC), !.
+
+
+permuteAnd(Condition, A, B, C):-
+			ABC =.. [Condition, A, B, C],
+			ACB =.. [Condition, A, C, B],
+			BAC =.. [Condition, B, A, C],
+			BCA =.. [Condition, B, C, A],
+			CBA =.. [Condition, C, B, A],
+			CAB =.. [Condition, C, A, B],
+			call(ABC), call(ACB), call(BAC), call(BCA), call(CBA), call(CAB).
+
+:-dynamic equalAngles/6.
+
+equalAngles(A, B, C, A, B, C).
+
+equalAngles(A, B, C, C, B, A).
+
 %axiom 1 Reflexivity of Congruence
-congruent(X, Y, Y, X).
+congruent(segment(X, Y), segment(Y, X)).
 
-congruent(X, Y, X, Y).
-congruent(Y, Y, X, X).
+congruent(segment(X, Y), segment(X, Y)).
+congruent(segment(Y, Y), segment(X, X)).
 
-congruent(a, b, a_, b_).
-congruent(b, c, b_, c_).
-congruent(a, c, a_, c_).
-
-congruent(x, y, x_, y_).
-congruent(x, u, x_, u_).
-congruent(u, y, u_, y_).
-congruent(y, z, y_, z_).
-congruent(u, z, u_, z_).
-
-isCongruent(A, B, C, D):-
-			congruent(A, B, C, D);
-			congruent(A, B, D, C);
-			congruent(B, A, C, D);
-			congruent(B, A, D, C), !.
-
-laysBetween(a, y, x).
-laysBetween(x, y, b).
+isCongruent(segment(A, B), segment(C, D)):-
+			permuteOrCongruence(congruent, segment(A, B), segment(C, D)), !.
 
 laysBetweenLaw(A, B, C):-
-			laysBetween(B, A, C);
-			laysBetween(A, B, C).
+			permuteOrLays(laysBetween, A, B, C).
+/*			laysBetween(B, A, C);
+			laysBetween(A, B, C).*/
 
 %equality of triangles on three sides
 equalityTriangles3(A, B, C, A_, B_, C_):-
-					isCongruent(A, B, A_, B_),
-					isCongruent(B, C, B_, C_),
-					isCongruent(A, C, A_, C_), !.
+					isCongruent(segment(A, B), segment(A_, B_)),
+					isCongruent(segment(B, C), segment(B_, C_)),
+					isCongruent(segment(A, C), segment(A_, C_)), !.
 
 isOnCircle(A, F, O):-
-				isCongruent(A, O, F, O), !.
+				isCongruent(segment(A, O), segment(F, O)), !.
 
 equalityArcs(A, B, C, D, F, O):-
 			isOnCircle(A, F, O),
 			isOnCircle(B, F, O),
 			isOnCircle(C, F, O),
 			isOnCircle(D, F, O),
-				isCongruent(A, B, C, D), !.
+				isCongruent(segment(A, B), segment(C, D)), !.
 
 %equality of angles
 equalityAngles1(A, B, C, A_, B_, C_):-
-		equalityTriangles3(A, B, C, A_, B_, C_), !.
+		equalityTriangles3(A, B, C, A_, B_, C_),
+		assert(equalAngles(A, B, C, A_, B_, C_)), !.
 
-equalityAngles2(A, B, C, A_, B_, C_, F, O):-
-		isOnCircle(A, F, O),
+
+equalityAnglesCircle(A, B, C, A_, B_, C_, F, O):-
 		isOnCircle(B, F, O),
-		isOnCircle(C, F, O),
-		isOnCircle(A_, F, O),
 		isOnCircle(B_, F, O),
-		isOnCircle(C_, F, O),
-			equalityArcs(A, C, A_, C_, F, O), !.
+			equalityArcs(A, C, A_, C_, F, O),
+				assert(equalAngles(A, B, C, A_, B_, C_)), !.
 
-equalityTriangles1(A, B, C, A_, B_, C_, F, O):-
-			isCongruent(A, B, A_, B_),
-			isCongruent(C, B, C_, B_),
-				equalityAngles2(A, B, C, A_, B_, C_, F, O), !.
+%tops of angles expected in 1 point
+equalityAnglesVertical(A, B, C, A_, B_, C_):-
+			laysBetweenLaw(A, A_, B),
+			laysBetweenLaw(C, C_, B),
+				assert(equalAngles(A, B, C, A_, B_, C_)), !;
 
-equalityTriangles2(A, B, C, A_, B_, C_, F, O):-
-		isCongruent(A, B, A_, B_),
-				equalityAngles2(B, A, C, B_, A_, C_, F, O),
-				equalityAngles2(A, B, C, A_, B_, C_, F, O), !;
+			laysBetweenLaw(A, C_, B),
+			laysBetweenLaw(C, A_, B), 
+				assert(equalAngles(A, B, C, A_, B_, C_)), !.
+
+equalityTriangles1(A, B, C, A_, B_, C_):-
+			isCongruent(segment(A, B), segment(A_, B_)),
+			isCongruent(segment(B, C), segment(B_, C_)),
+				equalAngles(A, B, C, A_, B_, C_), !.
+
+equalityTriangles2(A, B, C, A_, B_, C_):-
+		isCongruent(segment(A, B), segment(A_, B_)),
+				equalAngles(A, B, C, A_, B_, C_),
+				equalAngles(B, A, C, B_, A_,C_), !;
 		
-		isCongruent(B, C, B_, C_),
-				equalityAngles2(A, B, C, A_, B_, C_, F, O),
-				equalityAngles2(A, C, B, A_, C_, B_, F, O), !;
+		isCongruent(segment(B, C), segment(B_, C_)),
+				equalAngles(A, B, C, A_, B_, C_),
+				equalAngles(B, C, A, B_, C_,A_), !;
 		
-		isCongruent(A, C, A_, C_),
-				equalityAngles2(B, A, C, B_, A_, C_, F, O),
-				equalityAngles2(A, C, B, A_, C_, B_, F, O), !.
+		isCongruent(segment(A, C), segment(A_, C_)),
+				equalAngles(B, A, C, B_, A_, C_),
+				equalAngles(B, C, A, B_, C_, A_), !.
 
+equalityTriangles(A, B, C, A_, B_, C_):-
+			equalityTriangles1(A, B, C, A_, B_, C_), !;
+			equalityTriangles2(A, B, C, A_, B_, C_), !;
+			equalityTriangles3(A, B, C, A_, B_, C_), !.
 
 
 intersectCircle(A, B, F, O):-
@@ -97,29 +109,16 @@ intersectCircle(A, B, F, O):-
 			isInsideCircle(B, F, O),
 			not(isInsideCircle(A, F, O)), !.
 
-/*
-point(a).
-point(b).
-point(x).
-point(y).
-point(u).
-point(z).
-point(x_).
-point(y_).
-point(u_).
-point(z_).
-*/
-
 %axioms
 %axiom 2 Identity of Congruence
 identityCongruence(X, Y):-
-					isCongruent(X, Y, Z, Z), !.
+					isCongruent(segment(X, Y), segment(Z, Z)), !.
 
 %axiom 3 Transitivity of Congruence
 transitivityCongruence(X, Y, Z, U, V, W):-
-						isCongruent(X, Y, Z, U),
-						isCongruent(X, Y, V, W),
-						isCongruent(Z, U, V, W), !.
+						isCongruent(segment(X, Y), segment(Z, U)),
+						isCongruent(segment(X, Y), segment(V, W)),
+						isCongruent(segment(Z, U), segment(V, W)), !.
 
 %axiom 4 Identity of Betweenness
 identityBetweenness(X, Y):-
@@ -146,35 +145,37 @@ upperDimension(X, Y, Z):-
 					laysBetweenLaw(X, Y, Z);
 					laysBetweenLaw(X, Z, Y);
 					laysBetweenLaw(Z, Y, X);
-						isCongruent(X, U, X, V),
-						isCongruent(Y, V, Y, U),
-						isCongruent(Z, V, Z, U), !.
+						isCongruent(segment(X, U), segment(X, V)),
+						isCongruent(segment(Y, V), segment(Y, U)),
+						isCongruent(segment(Z, V), segment(Z, U)), !.
+
+
 
 %axiom 9.1 Axiom of Euclid
 axiomEuclid1(X, Y, Z):-
 				laysBetweenLaw(X, Y, Z), !;
 				laysBetweenLaw(Y, Z, X), !;
-				laysBetweenLaw(Z, X, y), !;
-					isCongruent(X, A, Y, A),
-					isCongruent(X, A, Z, A), !.
+				laysBetweenLaw(Z, X, Y), !;
+					isCongruent(segment(X, A), segment(Y, A)),
+					isCongruent(segment(X, A), segment(Z, A)), !.
 
 %axiom 9.2 Axiom of Euclid
 axiomEuclid2(X, Y, W, U, V, Z):-
 				laysBetweenLaw(X, W, Y),
 				laysBetweenLaw(X, V, U),
 				laysBetweenLaw(W, V, Z),
-					isCongruent(U, Y, V, Z),
-					isCongruent(X, Y, Y, W),
-					isCongruent(X, U, U, V),
-					isCongruent(V, Z, Z, W), !.
+					isCongruent(segment(U, Y), segment(V, Z)),
+					isCongruent(segment(X, Y), segment(Y, W)),
+					isCongruent(segment(X, U), segment(U, V)),
+					isCongruent(segment(V, Z), segment(Z, W)), !.
 
 %axiom 10 Five Segment
 fiveSegment(X, Y, U, Z, X_, Y_, U_, Z_):-	
 				laysBetweenLaw(X, Z ,Y),
 				laysBetweenLaw(X_, Z_, Y_),
-					not(isCongruent(X, Y, P, P)),
-					isCongruent(X, Y, X_, Y_),
-					isCongruent(Y, Z, Y_, Z_),
-					isCongruent(X, U, X_, U_),
-					isCongruent(Y, U, Y_, U_),
-					isCongruent(Z, U, Z_, U_), !.
+					not(isCongruent(segment(X, Y), segment(P, P))),
+					isCongruent(segment(X, Y), segment(X_, Y_)),
+					isCongruent(segment(Y, Z), segment(Y_, Z_)),
+					isCongruent(segment(X, U), segment(X_, U_)),
+					isCongruent(segment(Y, U), segment(Y_, U_)),
+					isCongruent(segment(Z, U), segment(Z_, U_)), !.
