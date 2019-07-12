2184b60a47f95468c94238910dca05c3c719eea2
Alexandra Shilova <alex.shilova04@inbox.ru>
2019-07-12 21:47:22 +0700

    ITS WORKSSSSS

--- Prolog/axiomsTarski.pl
+++ Prolog/axiomsTarski.pl
@@ -1,73 +1,89 @@
-prove_p(A) :-prove_p(A, P),write_proof(P).
-prove_p(true,[]).
+% display a proof tree
+prove_p(A):-prove_p(A,P),write_proof(P).
 
-prove_p((A, B),[p((A, B), (A:-C))|Proof]) :-
-	clause(A, C),
-	conj_append(C, B, D),
-	prove_p(D, Proof).
+% prove_p(A,P) <- P is proof tree of A
+prove_p(true,[]):-!.
+prove_p((A,B),[p((A,B),(A:-C))|Proof]):-!,
+	clause(A,C),
+	conj_append(C,B,D),
+	prove_p(D,Proof).
+prove_p(A,[p(A,(A:-B))|Proof]):-
+	clause(A,B),
+	prove_p(B,Proof).
 
-prove_p(A, [p(A, (A:-B))|Proof]) :-
-	clause(A, B),
-	prove_p(B, Proof).
-
-write_proof([]) :-
-	write('...............[]'), nl.
-
-write_proof([p(A, B)|Proof]) :-
+write_proof([]):-
+	write('...............[]'),nl.
+write_proof([p(A,B)|Proof]):-
 	write((:-A)),nl,
-	write(B),nl,
+	write('.....|'),write('..........'),write(B),nl,
+	write('.....|'),write('..................../'),nl,
 	write_proof(Proof).
 
-conj_append(true, Ys, Ys).
-conj_append(X, Ys, (X, Ys)) :-X\=true, X\=(_One, _TheOther).
-conj_append((X, Xs), Ys, (X, Zs)) :-conj_append(Xs, Ys, Zs).
+conj_append(true,Ys,Ys).
+conj_append(X,Ys,(X,Ys)):-X\=true,X\=(_One,_TheOther).
+conj_append((X,Xs),Ys,(X,Zs)):-conj_append(Xs,Ys,Zs).
+
 
 :-dynamic circle/2.
 :-dynamic point/1.
 :-dynamic laysBetween/3.
 :-dynamic congruent/2.
 
-isCongruent(segment(A, B), segment(A, B)).
-isCongruent(segment(A, B), segment(B, A)).
-isCongruent(segment(A, B), segment(C, D)) :-
-			congruent(segment(B, A), segment(C, D)).
-isCongruent(segment(A, B), segment(C, D)) :-
-			congruent(segment(A, B), segment(C, D)).
-isCongruent(segment(A, B), segment(C, D)) :-
-			congruent(segment(B, A), segment(D, C)).
-isCongruent(segment(A, B), segment(C, D)) :-
-			congruent(segment(A, B), segment(D, C)).
-isCongruent(segment(A, B), segment(C, D)) :-
-			congruent(segment(A, B), X),
-			isCongruent(X, segment(C, D)).
-%Euclid's Axiom: midline is equal to half of parallel edge
-isCongruent(segment(U, Y), segment(Z, A)) :-
-				laysBetweenLaw(A, B, Y), isCongruent(segment(A, Y), segment(Y, B)),
-				laysBetweenLaw(B, C, U), isCongruent(segment(B, U), segment(U, C)),
-				laysBetweenLaw(C, A, Z), isCongruent(segment(C, Z), segment(Z, A)).
-%fiveSegment
-isCongruent(segment(U, Z), segment(U_, Z_)):-
-				laysBetweenLaw(X, Z ,Y),
-				laysBetweenLaw(X_, Z_, Y_),
-					isCongruent(segment(Z, U), segment(Z_, U_)),
-					isCongruent(segment(X, Y), segment(X_, Y_)), isCongruent(segment(Y, Z), segment(Y_, Z_)),
-					isCongruent(segment(X, U), segment(X_, U_)), isCongruent(segment(Y, U), segment(Y_, U_)).
+laysBetweenLaw(X, X, X).
+
 laysBetweenLaw(A, B, C) :-
 	laysBetween(A, B, C).
 laysBetweenLaw(A, B, C) :-
 	laysBetween(B, A, C).
 
-laysBetweenLaw(X, X, Y) :- Y is X.
+congruented(segment(A, B), segment(B, A)).
+congruented(segment(A, B), segment(C, D)) :-
+			congruent(segment(A, B), segment(C, D)).
+congruented(segment(A, B), segment(C, D)) :-
+			congruent(segment(A, B), segment(D, C)).
+congruented(segment(A, B), segment(C, D)) :-
+			congruent(segment(B, A), segment(C, D)).
+congruented(segment(A, B), segment(C, D)) :-
+			congruent(segment(B, A), segment(D, C)).
 
-oneLine(A, B, C) :-
-	laysBetweenLaw(A, B, C).
-oneLine(A, B, C) :-
-	laysBetweenLaw(B, C, A).
-oneLine(A, B, C):-
-	laysBetweenLaw(A, C, B).
+congruented(segment(A, B), segment(C, D)) :-
+			congruent(segment(C, D), segment(A, B)).
+congruented(segment(A, B), segment(C, D)) :-
+			congruent(segment(C, D), segment(B, A)).
+congruented(segment(A, B), segment(C, D)) :-
+			congruent(segment(D, C), segment(A, B)).
+congruented(segment(A, B), segment(C, D)) :-
+			congruent(segment(D, C), segment(B, A)).
+
+% Transitivity of congruence
+transit(segment(A, B), segment(C, D)) :-
+			congruent(segment(A, B), X),
+			congruented(segment(C, D), X),
+			assert(congruent(segment(A, B), segment(C, D))).
+
+axiomEuclid2(segment(U, Y), segment(Z, A)) :-
+				laysBetweenLaw(A, B, Y), congruented(segment(A, Y), segment(Y, B)),
+				laysBetweenLaw(B, C, U), congruented(segment(B, U), segment(U, C)),
+				laysBetweenLaw(C, A, Z), congruented(segment(C, Z), segment(Z, A)),
+				assert(congruent(segment(U, Y), segment(Z, A))).
+
+fiveSegment(segment(U, Z), segment(U_, Z_)):-
+				laysBetweenLaw(X, Z ,Y),
+				laysBetweenLaw(X_, Z_, Y_),
+				congruented(segment(X, Y), segment(X_, Y_)),
+				congruented(segment(Y, Z), segment(Y_, Z_)),
+				congruented(segment(X, U), segment(X_, U_)),
+				congruented(segment(Y, U), segment(Y_, U_)),
+				assert(congruent(segment(Z, U), segment(Z_, U_))).
+
+isCongruent(segment(A, B), segment(C, D)):-
+		fiveSegment(segment(A, B), segment(C, D));
+		axiomEuclid2(segment(A, B), segment(C, D));
+		transit(segment(A, B), segment(C, D));
+		congruented(segment(A, B), segment(C, D)).
 
 identityCongruence(X, Y) :-
-			isCongruent(segment(X, Y), segment(Z, Z)).
+			congruented(segment(X, Y), segment(Z, Z)).
 identityBetweenness(X, Y) :-
 					laysBetweenLaw(X, X, Y).
 axiomPasch(U, Y, V, X) :-
